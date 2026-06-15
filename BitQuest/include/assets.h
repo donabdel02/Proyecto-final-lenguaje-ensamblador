b8af5bf9cf23de47affd6eaf4d8795a26c716069
donabdel02 <abdeonzo9812@gmail.com>
2026-06-15 00:49:01 -0600

    corexcion ascii

--- BitQuest/include/assets.h
+++ BitQuest/include/assets.h
@@ -15,7 +15,7 @@
 //con ASCII para el punto extra y para que se vea más bonito el juego, aunque no es necesario para el funcionamiento del mismo
 #define VISUAL_PARED   "##"
 #define VISUAL_LIBRE   "  "
-#define VISUAL_JUGADOR "/\\"
+#define VISUAL_JUGADOR "/\"
 #define VISUAL_MONEDA  "o "
 #define VISUAL_LLAVE   "! "
 #define VISUAL_PUERTA  "[]"
